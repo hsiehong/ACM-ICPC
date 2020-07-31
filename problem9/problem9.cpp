941f4fd29764508ba59f0412cbcd1c42f800aeed
hsiehong <hsiehong1022@gmail.com>
2020-07-31 10:26:46 +0800

    problem 9

--- problem9/problem9.cpp
+++ problem9/problem9.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define N 9.59717124788752402
 
 using namespace std;
 
 int main(int argc,char** argv){
-	const double alpha=19.194;
-	double ans;
 	int ncase,b;
-	cin>>ncase;
+	scanf("%d",&ncase);
 	while(ncase--){
-		cin>>b;
-		ans=alpha*b/2;
-		int t=(int)(ans*10);
-		if(t%10>0)ans+=1;
-		cout<<(int)ans<<endl;
+		scanf("%d",&b);
+		printf("%d\n",(int)ceil(N*(double)b));
 	}
 	return 0;
 }
